02172842d30876ef024a8a75c4ac11a01c8472ef
MrPancakers <br3ttscott@gmail.com>
2019-02-16 13:56:37 +1000

    Moving keybinds to their own file

--- client_packages/swift-core/index.js
+++ client_packages/swift-core/index.js
@@ -3,7 +3,3 @@ require('swift-core/hud');
 require('swift-core/finance');
 
 mp.events.call('enableLoginCamera');
-
-mp.keys.bind(0x72, true, function() {   // F3
-    mp.gui.cursor.visible = !mp.gui.cursor.visible;
-});
