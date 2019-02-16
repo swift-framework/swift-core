3a2ad3525f97dc74dd7ed669ac6dfd9720c619bf
MrPancakers <br3ttscott@gmail.com>
2019-02-16 14:13:08 +1000

    Changed hud rendering

--- client_packages/swift-core/hud/index.js
+++ client_packages/swift-core/hud/index.js
@@ -1,4 +1,15 @@
+let money;
+let enableHud = false;
+
+mp.events.addDataHandler('loggedIn', () => {
+    money = mp.players.local.getVariable('money');
+    enableHud = true;
+});
+
+mp.events.addDataHandler('money', (entity, value) => {
+    money = value;
+});
+
 mp.events.add('render', () => {
-    let money = mp.players.local.getVariable('money');
-    if(mp.players.local.getVariable('loggedIn')) mp.game.graphics.drawText(`~g~$~w~${money}`, [0.19, 0.81], { font: 7, color: [255,255,255,255], scale: [0.8, 0.8] });
+    if(enableHud) mp.game.graphics.drawText(`~g~$~w~${money}`, [0.19, 0.81], { font: 7, color: [255,255,255,255], scale: [0.8, 0.8] });
 });
