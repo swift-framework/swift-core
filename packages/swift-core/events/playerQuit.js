9627ad333b4c9ae099459353a96ac50c43b6703e
MrPancakers <br3ttscott@gmail.com>
2019-02-17 16:04:11 +1000

    Pos was commented out, added console log for quitting

--- packages/swift-core/events/playerQuit.js
+++ packages/swift-core/events/playerQuit.js
@@ -2,6 +2,7 @@ mp.events.add('playerQuit', (player) => {
     if(player.deathTimer) clearTimeout(player.deathTimer);
     if(player.loggedInAs != ''){
         swift.auth.saveAccount(player);
+        console.log(`${swift.chalk.red(player.name)} has quit the server.`);
     } else {
         clearTimeout(player.idleKick);
     }
