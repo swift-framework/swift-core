03400aefe64b179d86d0797fd0ccddddb5f754b2
MrPancakers <br3ttscott@gmail.com>
2019-02-19 07:41:47 +1000

    Removed unused deposit function

--- packages/swift-core/finance.js
+++ packages/swift-core/finance.js
@@ -4,9 +4,6 @@ mp.blips.new(108, new mp.Vector3(230.05, 214.36, 0), { name: 'Bank', color: 2, s
 module.exports = {
     generateBankID: function(){
         return Math.floor(Math.random() * (999999999 - 100000000) ) + 100000000;
-    },
-    depositMoney: function(user, amount){
-        if(user.data.money < amount) return user.outputChatBox(`${swift.prefix.server} You do not have that much to deposit`);
     }
 };
 
