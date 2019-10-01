873925133fbc404fcb8fcdd67023c3250fb0bbd3
MrPancakers <br3ttscott@gmail.com>
2019-10-01 10:44:13 +1000

    Added documentation & fixed RP chat commands without text

--- packages/swift-core/chat.js
+++ packages/swift-core/chat.js
@@ -13,21 +13,26 @@ mp.events.add('playerChat', (player, text) => { //  Local chat
 
 mp.events.addCommand({
     's': (player, text) => {
+        if(!text) return player.outputChatBox(`${swift.prefix.syntax} /s [text]`);
         rangeChat(player, 20, text, 'shouts');
     },
     'w': (player, text) => {
+        if(!text) return player.outputChatBox(`${swift.prefix.syntax} /w [text]`);
         rangeChat(player, 5, text, 'whispers');
     },
     'b': (player, text) => {
         if(player.data.mute) return player.outputChatBox(`${swift.prefix.error} You cannot talk while you are muted.`);
+        if(!text) return player.outputChatBox(`${swift.prefix.syntax} /b [text]`);
         mp.players.broadcastInRange(player.position, 10, `!{#bfbfbf}(( ${player.name} )): ${text}`);
     },
     'me': (player, text) => {
         if(player.data.mute) return player.outputChatBox(`${swift.prefix.error} You cannot talk while you are muted.`);
+        if(!text) return player.outputChatBox(`${swift.prefix.syntax} /me [text]`);
         mp.players.broadcastInRange(player.position, 10, `!{#d19aed}* ${player.name} ${text}`);
     },
     'do': (player, text) => {
         if(player.data.mute) return player.outputChatBox(`${swift.prefix.error} You cannot talk while you are muted.`);
+        if(!text) return player.outputChatBox(`${swift.prefix.syntax} /do [text]`);
         mp.players.broadcastInRange(player.position, 10, `!{#d19aed}* ${text} (( ${player.name} ))`);
     }
 });
--- packages/swift-core/wrapper/player.js
+++ packages/swift-core/wrapper/player.js
@@ -1,19 +1,74 @@
-//  This stops confusion between the RAGEMP API and Swift API
-//  Variable names should be 'swift:{variableName}' again to keep consistent
-
 /**
  *  Returns the players money amount.
- *  -   This helps developers from having to find out what the money variable is.
+ *  This helps developers from having to find out what the money variable is.
+ *  @return {int} Amount of money
  */
 mp.Player.prototype.getMoney = function(){
     return this.getVariable('swift:money');
 };
 
+/**
+ *  Sets the player's money (Do not get this confused with addMoney()).
+ *  @param {int} amount - Amount of money to set
+ *  @example
+ *      player.setMoney(5000);
+ */
 mp.Player.prototype.setMoney = function(amount){
-    if(isNaN(amount) || amount < 0) return;
-    return this.setVariable('swift:money', parseInt(amount));
+    if(isNaN(amount) || amount < 0) return this.outputChatBox(`${swift.prefix.error} You must enter a valid number.`);
+    this.setVariable('swift:money', parseInt(amount));
 };
 
+/**
+ *  Add's money on top of the player's current amount.
+ *  @param {int} amount - Amount of money to add
+ *  @example
+ *      player.addMoney(5000);
+ */
+mp.Player.prototype.addMoney = function(amount){
+    if(isNaN(amount) || amount < 0) return this.outputChatBox(`${swift.prefix.error} You must enter a valid number.`);
+    let money = this.getMoney() + parseInt(amount);
+    this.setVariable('swift:money', money);
+};
+
+/**
+ *  Removes money off of the player's current amount.
+ *  @param {int} amount - Amount of money to remove
+ *  @example
+ *      player.removeMoney(5000);
+ */
+mp.Player.prototype.removeMoney = function(amount){
+    if(isNaN(amount) || amount < 0) return this.outputChatBox(`${swift.prefix.error} You must enter a valid number.`);
+    if(this.getMoney < amount) return false;
+    let money = this.getMoney() - parseInt(amount);
+    this.setVariable('swift:money', money);
+};
+
+/**
+ *  Sends money from one player to another player.
+ *  @param {object} target - The target player you want to send the money to
+ *  @param {int} amount - The amount of money to send to the target player
+ */
+mp.Player.prototype.sendMoney = function(target, amount){
+    if(isNaN(amount) || amount < 0) return this.outputChatBox(`${swift.prefix.error} You must enter a valid number.`);
+    if(this.getMoney < amount) return this.outputChatBox(`${swift.prefix.error} You do not have enough money to send.`);
+    target.addMoney(amount);
+    this.removeMoney(amount);
+};
+
+/**
+ *  Returns the group the player is apart of
+ *  @return {int} Group ID
+ */
 mp.Player.prototype.getGroup = function(){
     return this.getVariable('swift:group');
+};
+
+/**
+ *  Sets the group ID of the player
+ *  @param {int} id - Group ID
+ */
+mp.Player.prototype.setGroup = function(id){
+    if(swift.group.getGroup(id) == undefined) return this.outputChatBox(`${swift.prefix.error} Group failed to change (does not exist).`);
+    this.setVariable('swift:group', id);
+    this.outputChatBox(`${swift.prefix.info} Your group has been updated.`);
 };
